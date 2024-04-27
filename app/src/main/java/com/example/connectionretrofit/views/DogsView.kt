139df38dffe883df84af8cf1825ccb7a1edf64bc
Javi <luis.zapata01@iest.edu.mx>
2024-04-26 18:33:33 -0600

    second commit: implementacion de visualizacion de las imagenes

--- app/src/main/java/com/example/connectionretrofit/views/DogsView.kt
+++ app/src/main/java/com/example/connectionretrofit/views/DogsView.kt
@@ -3,12 +3,16 @@ package com.example.connectionretrofit.views
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.connectionretrofit.viewmodels.DogViewModel
 
 //@Preview(showBackground = true)
@@ -20,10 +24,13 @@ fun DogsView(dogViewModel: DogViewModel){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
         ){
-        Text(text = "Imagen de la URL es: ${image}")
+        AsyncImage(model = "${image}", contentDescription = null,
+        modifier = Modifier.height(300.dp).width(300.dp))
+
+        //Text(text = "Imagen de la URL es: ${image}")
 
         Button(onClick = {dogViewModel.fetchData()}){
-            Text(text = "Buscar otra imagen")
+            Text(text = "Dame otro perro")
         }
     }
 }
